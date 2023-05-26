62dac29f934044b76ca8d84ff76630d3298c2678
phantomMask216 <108537113+phantomMask216@users.noreply.github.com>
2023-05-26 17:17:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask
-  
 app = Flask(__name__)
-  
-@app.route("/")
-def hello():
-    return "Hello Geeks!! from Google Colab"
-  
-app.run()
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
