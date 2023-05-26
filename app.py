d35f1f796e5e530bb50796a799913668ec174dfb
phantomMask216 <108537113+phantomMask216@users.noreply.github.com>
2023-05-26 17:27:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/webhook',methods=["POST"])
+def hook():
+    return 'webhook'
+    print(request.data)
